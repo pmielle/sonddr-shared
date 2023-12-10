eee2bc051613dfc041d62aa57ae63ec1ca8715d0
Paul Mielle <pm.mielle@gmail.com>
2023-12-10 15:21:07 +0100

    isChange init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 export * from "./types.js";
 
 export function makeCheerId(ideaId: string, userId: string): string {
-    return `${ideaId}-${userId}`;
+	return `${ideaId}-${userId}`;
 }
 
 export function makeVoteId(commentId: string, userId: string): string {
-    return `${commentId}-${userId}`;
+	return `${commentId}-${userId}`;
+}
+
+export function isChange(obj: any): boolean {
+	return 'docId' in obj && 'type' in obj;
 }
 
 export const placeholder_id = "PLACEHOLDER_ID";
