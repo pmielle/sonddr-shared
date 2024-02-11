272299054214f6d81d0eb0dd89a60b436c8cdfc3
Paul Mielle <pm.mielle@gmail.com>
2024-02-11 12:20:31 +0100

    back to server

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,4 @@
-import { DbUser, User } from "./types.js";
-
 export * from "./types.js";
-
-
-
-export function reviveUser(dbDoc: DbUser, userId: string|undefined): User {
-	return reviveUsers([dbDoc], userId)[0];
-}
-
-export function reviveUsers(dbDocs: DbUser[], userId: string|undefined): User[] {
-
-    if (dbDocs.length == 0) { return []; }
-
-    // convert dbDocs into docs
-    const docs: User[] = dbDocs.map((dbDoc) => {
-        dbDoc["isUser"] = userId === undefined ? undefined : dbDoc.id === userId;
-        return dbDoc as any;
-    });
-
-    // return
-    return docs;
-
-}
 
 export function makeCheerId(ideaId: string, userId: string): string {
 	return `${ideaId}-${userId}`;
