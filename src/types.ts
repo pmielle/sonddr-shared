e43420aed9c2f0462c1e698e62aa7a3924a98ad4
Paul Mielle <pm.mielle@gmail.com>
2024-03-17 13:40:27 +0100

    isUser is required

--- src/types.ts
+++ src/types.ts
@@ -79,7 +79,7 @@ export type User = {
     date: Date,
     externalLinks: ExternalLink[],
     bio: string,
-    isUser?: boolean,
+    isUser: boolean,
 };
 
 export const externalLinkTypes = ["instagram", "discord", "slack", "googledrive"] as const;
